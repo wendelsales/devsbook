4952d2bef4ffda3b8db79f59ffc9382eeabbc0c1
wendelsales <uendel.1@hotmail.com>
2022-03-27 23:19:12 -0300

    correção

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,6 @@ $router = new Router();
 
 $router->get('/', 'HomeController@index');
 $router->get('/login', 'LoginController@signin'); /*processo de login*/
-$router->get('/login', 'LoginController@signinAction');
+$router->post('/login', 'LoginController@signinAction');
 $router->get('/cadastro', 'LoginController@signup'); /*cadastro*/
 
